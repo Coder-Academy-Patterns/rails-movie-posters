12f634e128c9a350d25dd2b675367413814538a3
Patrick Smith <patrick@burntcaramel.com>
2017-10-18 12:46:50 +1100

    Add poster image uploading to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
+  include PosterImageUploader[:poster_image]
+  # Add the following:
+  # .poster_image_data
+  # .poster_image
+  # .poster_image_url
+  
   validates :title, presence: true
   validates :year_released, numericality: { only_integer: true, greater_than: 1800, less_than: 2100 }
 end
